4f986be625a36f5fc5769d2905a72839ab550503
Jules Loïc <julesloic9@gmail.com>
2025-06-04 02:16:02 +0200

    frontent web react js

---
+++ web/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Bonjour tous le monde !</p>
+    </>
+  )
+}
+
+export default App
