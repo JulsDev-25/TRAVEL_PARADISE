600458dc59f121aca43be7d3bd1c6bf8400bf18c
Jules Loïc <julesloic9@gmail.com>
2025-06-14 23:00:40 +0200

    crud guide

---
+++ webApp/src/Form/GuideForm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Guide;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GuideForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('photo')
+            ->add('statut')
+            ->add('pays')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Guide::class,
+        ]);
+    }
+}
